410f6be4fd79474a4bfa025203905c60f36ae068
arjun289 <arjun289singh@gmail.com>
2019-05-08 15:29:33 +0800

    Remove unncecessary images and styles

--- lib/acqdat_web/views/layout_view.ex
+++ lib/acqdat_web/views/layout_view.ex
@@ -1,4 +1,10 @@
 defmodule AcqdatWeb.LayoutView do
   use AcqdatWeb, :view
   alias AcqdatWeb.Guardian
+
+  def current_user(conn) do
+    user = conn.assigns.current_user
+    user.first_name
+  end
+
 end
