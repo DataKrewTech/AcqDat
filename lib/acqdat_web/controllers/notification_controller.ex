5180059d5dfbd1fad765ce1354b0cdf6020fc209
arjun289 <arjun289singh@gmail.com>
2019-06-07 17:30:55 +0800

    Add bootstrap select notify

--- lib/acqdat_web/controllers/notification_controller.ex
+++ lib/acqdat_web/controllers/notification_controller.ex
@@ -1,14 +1,17 @@
 defmodule AcqdatWeb.NotificationController do
   use AcqdatWeb, :controller
 
-  alias Acqdat.Model.SensorNotification
+  alias Acqdat.Model.{SensorNotification, Device}
+  alias Acqdat.Schema.SensorNotifications, as: SN
   def index(conn, _) do
     notifications = SensorNotification.get_all()
     render(conn, "index.html", notifications: notifications)
   end
 
   def new(conn, _) do
-    render(conn, "new.html")
+    changeset = SN.changeset(%SN{}, %{})
+    devices = Device.get_all()
+    render(conn, "new.html", changeset: changeset, devices: devices)
   end
 
   def edit(conn, %{"id" => id}) do
