bc3ff836ae6daa42f6136ead2b32281cdf656364
arjun289 <arjun289singh@gmail.com>
2019-06-26 18:11:59 +0800

    Added index and detail pages for data trace

--- lib/acqdat/domain/data_trace/cron_task.ex
+++ lib/acqdat/domain/data_trace/cron_task.ex
@@ -1,5 +1,6 @@
 defmodule Acqdat.DataTrace.CronTask do
   use Task
+  alias Acqdat.Repo
   alias Acqdat.Model.SensorData
 
   def start_link(_args) do
@@ -9,7 +10,7 @@ defmodule Acqdat.DataTrace.CronTask do
   def cron() do
     receive do
     after
-      900_000 ->
+      300_000 ->
         send_data()
         cron()
         # code
@@ -17,11 +18,14 @@ defmodule Acqdat.DataTrace.CronTask do
   end
 
   def send_data() do
-    start_time = Timex.shift(DateTime.utc_now(), minutes: -15)
+    start_time = Timex.shift(DateTime.utc_now(), minutes: -5)
     end_time = DateTime.utc_now()
 
-    case SensorData.get_by_time_range(start_time, end_time) do
-      [] ->
+    start_time
+    |> SensorData.get_by_time_range(end_time)
+    |> extract_data()
+    |> case do
+      nil ->
         :ok
 
       data ->
@@ -30,6 +34,28 @@ defmodule Acqdat.DataTrace.CronTask do
   end
 
   defp data_trace(data) do
-    HTTPoison.post(url, body, headers \\ [], options \\ [])
+    body = Jason.encode!(data)
+    headers = [{"Accept", "application/json"}]
+    url = "https://innovfest19-bl.herokuapp.com/post"
+    HTTPoison.post(url, body, headers)
+  end
+
+  # TODO: needs to be refactored once proper provDat is up.
+  defp extract_data(query) do
+    case Repo.all(query) do
+      nil ->
+        nil
+
+      data ->
+        result = List.first(data)
+
+        %{
+          "Device_ID" => result.sensor.device.id,
+          "Timestamp" => result.inserted_timestamp,
+          "Sensor" => %{
+            "#{result.sensor.name}" => result.datapoint
+          }
+        }
+    end
   end
 end
--- lib/acqdat/model/sensor_data.ex
+++ lib/acqdat/model/sensor_data.ex
@@ -4,17 +4,16 @@ defmodule Acqdat.Model.SensorData do
   data.
   """
   import Ecto.Query
-  alias Acqdat.Repo
   alias Acqdat.Schema.SensorData
 
+  @doc """
+  Returns `query` for getting sensor data by `start_time` and `end_time`.
+  """
   def get_by_time_range(start_time, end_time) do
-    query =
-      from(
-        data in SensorData,
-        where: data.inserted_at >= ^start_time and data.inserted_at <= ^end_time,
-        preload: [sensor: :device]
-      )
-
-    Repo.all(query)
+    from(
+      data in SensorData,
+      where: data.inserted_at >= ^start_time and data.inserted_at <= ^end_time,
+      preload: [sensor: :device]
+    )
   end
 end
