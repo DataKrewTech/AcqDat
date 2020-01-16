48317ce81387bc4c9f2f48c33c7a6536171cfdca
arjun289 <arjun289singh@gmail.com>
2020-01-16 15:48:16 +0800

    Change user_uuid key to employee_uuid
    Tool management modficiations

--- lib/acqdat_web/controllers/api/tool-management/tool_management_controller.ex
+++ lib/acqdat_web/controllers/api/tool-management/tool_management_controller.ex
@@ -34,7 +34,7 @@ defmodule AcqdatWeb.API.ToolManagementController do
   alias Acqdat.Context.ToolManagement
 
   def verify_employee(conn, params) do
-    %{"user_uuid" => uuid} = params
+    %{"employee_uuid" => uuid} = params
     {status, data} = ToolManagement.verify_employee(%{card_uuid: uuid})
     render(conn, "employee_verified.json", status: status, data: data)
   end
